0d8873eb0e9a29722db330c3c48d32f1eb380fa2
Sultan Ali <sultan.arshi@gmail.com>
2023-01-26 16:40:51 +0100

    Initial Commit

---
+++ VidoeEditingDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VidoeEditingDemo
+//
+//  Created by Rashdan Natiq on 25/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
